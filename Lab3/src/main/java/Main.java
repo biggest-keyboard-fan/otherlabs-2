62ffadb749a87bb776abab32a2e7ecbc8f1b93a0
biggest-keyboard-fan <nyancapt@gmail.com>
2020-05-12 18:03:30 +0300

    Lab3 Refactoring

--- Lab3/src/main/java/Main.java
+++ Lab3/src/main/java/Main.java
@@ -8,38 +8,38 @@ public class Main {
     static volatile AtomicInteger
             activeThread = new AtomicInteger(0);
     static AtomicInteger writeCount = new AtomicInteger(0);
-    public static void main(String[] args) throws IOException {
+    static BufferedWriter writer;
+    static Thread[] threads;
+    static int waitTime = 30;
+    public static void main(String[] args) throws IOException, InterruptedException {
         File writeFile = new File("write.txt");
         if(writeFile.exists()) writeFile.delete();
         writeFile.createNewFile();
-        //TODO: create & clear file on start
-        Thread[] threads = new Thread[2];
+        writer = new BufferedWriter(new FileWriter(writeFile));
+        threads = new Thread[2];
         for (int i = 0; i < 2; i++) {
             //Create effectively final index variable
             int finalI = i;
             threads[i] = new Thread(()->{
-                while (true){
-                    try {
-                        Thread.sleep(30);
-                        if (activeThread.get() == finalI) {
-                            if (activeThread.get() == -1) break;
-                            String fileContent;
-                            try (Scanner scanner = new Scanner(new FileInputStream(writeFile)).useDelimiter("\\Z")) {
-                                fileContent = scanner.hasNext() ? scanner.next() : "";
-                            }
-                            try (FileWriter writer = new FileWriter(writeFile);) {
-                                String writeText = fileContent + "\n" + writeCount.get();
-                                System.out.println(finalI+" Writing to file");
-                                writer.write(writeText);
-                            }
-                            activeThread.set(finalI == 0 ? 1 : 0);
-                            if (writeCount.incrementAndGet() == 20) activeThread.set(-1);
+                try {
+                    for (int j = 0; j < 20; j++) {
+                        String writeText = "\n" + writeCount.getAndIncrement();
+                        System.out.println(finalI+" Writing to file "+new Date().getTime());
+                        writer.write(writeText);
+                        Thread.sleep(waitTime*2);
                     }
-                    }
-                    catch (Exception e) { e.printStackTrace(); }
                 }
+                catch (Exception e) { e.printStackTrace(); }
             });
         }
-        Arrays.stream(threads).forEach(Thread::start);
+        Arrays.stream(threads).forEach((t)->{
+            try {
+                t.start();
+                Thread.sleep(waitTime);
+            }catch (InterruptedException ignored){}
+        });
+        Thread.sleep(waitTime*20*2+500);
+        try { writer.close(); }
+        catch (IOException e) { e.printStackTrace(); }
     }
 }
