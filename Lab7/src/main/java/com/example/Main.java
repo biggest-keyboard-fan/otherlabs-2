74d9afaa04e5c0f1728a2a18dfc24d1497d2ba2d
biggest-keyboard-fan <nyancapt@gmail.com>
2020-05-13 13:29:11 +0300

    Lab7 fix2

--- Lab7/src/main/java/com/example/Main.java
+++ Lab7/src/main/java/com/example/Main.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Arrays.stream(new Human[]{new Human("Paul", new OfficeWorker()), new Human("Greg", new RegularWorker()), new Human("Bob", new Student())}).forEach((human -> {
-            human.getWork().wakeUp();
-            human.getWork().eat();
-            human.getWork().goToWork();
+            IGoingToWork goingToWork = human.getWork();
+            goingToWork.wakeUp();
+            goingToWork.eat();
+            goingToWork.goToWork();
         }));
     }
 }
